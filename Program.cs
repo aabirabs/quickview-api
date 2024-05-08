cdeea57a6045080c76ea928e86f9d28edc978deb
Abir ABASSI <abir170792@gmail.com>
2024-05-08 10:31:22 +0100

    GetDevices from prtg server

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using PrtgAPI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader();
+                      });
+});
+
+var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapGet("/", () => {
+
+   var client = new PrtgClient("http://127.0.0.1", "prtgadmin", "prtgadmin",ignoreSSL: true);
+    client.RetryDelay = 3;
+    var devices = client.GetDevices();
+    return devices;
+});
+
+app.Run();
